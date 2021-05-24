2b7f95343cd908b7dc30f57b56228e607ee5eba5
Brett Hiltbrand <bjhiltbrand@gmail.com>
2021-05-23 23:18:54 -0600

    Fixed a number of errors.

--- custom_components/drone_mobile/lock.py
+++ custom_components/drone_mobile/lock.py
@@ -30,23 +30,23 @@ class Lock(DroneMobileEntity, LockEntity):
 
     async def async_lock(self, **kwargs):
         """Locks the vehicle."""
-        if self.is_locked:
-            return
+        #if self.is_locked:
+        #    return
         _LOGGER.debug("Locking %s", self.coordinator.data['vehicle_name'])
         response = await self.coordinator.hass.async_add_executor_job(
             self.coordinator.vehicle.lock, self.coordinator.data["device_key"]
         )
-        self.coordinator.update_data_from_response(response)
+        self.coordinator.update_data_from_response(self.coordinator, response)
 
     async def async_unlock(self, **kwargs):
         """Unlocks the vehicle."""
-        if not self.is_locked:
-            return
+        #if not self.is_locked:
+        #    return
         _LOGGER.debug("Unlocking %s", self.coordinator.data['vehicle_name'])
         response = await self.coordinator.hass.async_add_executor_job(
             self.coordinator.vehicle.unlock, self.coordinator.data["device_key"]
         )
-        self.coordinator.update_data_from_response(response)
+        self.coordinator.update_data_from_response(self.coordinator, response)
 
     @property
     def is_locked(self):
